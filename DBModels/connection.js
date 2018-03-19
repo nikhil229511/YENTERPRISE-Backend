23de2ebe1916653bb1ab3bd105bd32fdcc2a470d
jayesh <jayeshvalbhani1711@gmail.com>
2018-03-19 20:16:57 +0530

    connected to postgres

--- DBModels/connection.js
+++ DBModels/connection.js
@@ -3,25 +3,29 @@ var connection;
 var pg=require('pg');
 module.exports={
     connect : function(){
+    // connection = new pg.Client('postgres://zsxoexqbgvzuij:1198bc5ba815f3d369239897c4ca91b53bd58ce31fad8bf92328c4309cb72c55@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d85ib48u7j6fh7');
+    //connection.connect();
+     connection = new pg.Client({
+        user: "zsxoexqbgvzuij",
+        password: "1198bc5ba815f3d369239897c4ca91b53bd58ce31fad8bf92328c4309cb72c55",
+        database: "d85ib48u7j6fh7",
+        port: 5432,
+        host: "ec2-54-243-210-70.compute-1.amazonaws.com",
+        ssl: true,
+        dialect: 'postgres'
+    }); 
+    connection.connect();
 
 
-    var conString = "postgres://zsxoexqbgvzuij:1198bc5ba815f3d369239897c4ca91b53bd58ce31fad8bf92328c4309cb72c55@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d85ib48u7j6fh7";
-    connection = new pg.Client(conString);
-    // connection.connect();
-    // var client = new pg.Client({
-    //     user: "zsxoexqbgvzuij",
-    //     password: "1198bc5ba815f3d369239897c4ca91b53bd58ce31fad8bf92328c4309cb72c55",
-    //     database: "d85ib48u7j6fh7",
-    //     port: 5432,
-    //     host: "ec2-54-243-210-70.compute-1.amazonaws.com",
-    //     ssl: true
-    // }); 
-    // client.connect();
-        
-        return connection;
+
+   
+    return connection;
     },
     testConnection: function(){
-        // connection.authenticate().then(()=>{
+        const query=connection.query('drop table if exists public.test11');
+        query.testConnection;
+        
+        // .complete(()=>{
         //     console.log('Actually connected');
         // })
         // .catch((err)=>{
