6f13f7e9b25ebf2995c3d658ded8fed7aa7c957a
jayesh <jayeshvalbhani1711@gmail.com>
2018-03-19 19:38:10 +0530

    connected to postgres

--- DBModels/connection.js
+++ DBModels/connection.js
@@ -1,26 +1,31 @@
 var Sequelize = require('sequelize');
 var connection;
-
+var pg=require('pg');
 module.exports={
     connect : function(){
-        connection=new Sequelize('yashikaenterprise','root','',{
-        host: 'localhost',
-        dialect:'mysql',
-        pool:{
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-            }
-        });
+
+
+    var conString = "postgres://zsxoexqbgvzuij:1198bc5ba815f3d369239897c4ca91b53bd58ce31fad8bf92328c4309cb72c55@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d85ib48u7j6fh7";
+    connection = new pg.Client(conString);
+    // connection.connect();
+    // var client = new pg.Client({
+    //     user: "zsxoexqbgvzuij",
+    //     password: "1198bc5ba815f3d369239897c4ca91b53bd58ce31fad8bf92328c4309cb72c55",
+    //     database: "d85ib48u7j6fh7",
+    //     port: 5432,
+    //     host: "ec2-54-243-210-70.compute-1.amazonaws.com",
+    //     ssl: true
+    // }); 
+    // client.connect();
+        
         return connection;
     },
     testConnection: function(){
-        connection.authenticate().then(()=>{
-            console.log('Actually connected');
-        })
-        .catch((err)=>{
-            console.log('error : ',err);
-        })
+        // connection.authenticate().then(()=>{
+        //     console.log('Actually connected');
+        // })
+        // .catch((err)=>{
+        //     console.log('error : ',err);
+        // })
     }    
 };
--- routes/login.js
+++ routes/login.js
@@ -14,9 +14,9 @@ router.get('/',function(req,res){
 
 router.post('/',function(req,res){
     
-  res.end('{"user_id":1,"username":"admin","password":"admin123","fName":"Jayesh","lName":"Doshi","user_type":"admin","financialYear":"2018-01-01","createdAt":"2018-01-01T00:00:00.000Z","updatedAt":"2018-01-01T00:00:00.000Z"}');
- /*   username=req.body.username;
-      password=req.body.password;
+  //res.end('{"user_id":1,"username":"admin","password":"admin123","fName":"Jayesh","lName":"Doshi","user_type":"admin","financialYear":"2018-01-01","createdAt":"2018-01-01T00:00:00.000Z","updatedAt":"2018-01-01T00:00:00.000Z"}');
+    username='admin';
+    password='admin123';
     model.loginMaster().findAll({ where: { username: username, password:password } }).then(function(result,err){
       if(err)
       throw err;
@@ -26,10 +26,10 @@ router.post('/',function(req,res){
         return null;
       }
       else{
-        //console.log(result[0]);
+        console.log(result[0]);
         console.log('connected');
         res.end(JSON.stringify(result[0]));
       }
-    });*/
+    });
 });
 module.exports = router;
